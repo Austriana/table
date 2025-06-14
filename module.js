9c428aa88d6e406dfa6a6747a135e381d71c068e
Austriana <austriana@email.com>
2025-06-15 01:44:50 +0200

    some clearer code with module i love it <3

--- module.js
+++ module.js
@@ -1,4 +1,4 @@
-export function myPlaceholder(){
+let myPlaceholder = () => {
     let art = document.getElementById('art').value;
     let myInput = document.getElementsByName('wert')[0];
 
@@ -30,4 +30,143 @@ export function myPlaceholder(){
         default:
             myInput.placeholder='Wert';
     }
+};
+
+const time = () => {
+    let timeElapsed = Date.now();
+    let time = new Date(timeElapsed);
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    if (hour < 10){
+        hour = `0${hour}`;
+    }
+    if (min < 10){
+        min = `0${min}`;
+    }
+    if (sec < 10){
+        sec = `0${sec}`;
+    }
+    return (`${hour}:${min}:${sec}`);
+};
+
+let createInputTable = (art, kategorie, time, tableRowId) => {
+    if (art !== "" && kategorie !== ""){
+        let inputTable = document.getElementById('inputList');
+        let row = inputTable.insertRow(1);
+        row.id=tableRowId;
+        console.log(tableRowId);
+        let cellOne = row.insertCell(0);
+        let cellTwo = row.insertCell(1);
+        let cellThree = row.insertCell(2);
+        cellOne.innerText = time();
+        cellTwo.innerText = art;
+        cellThree.innerText = kategorie;
+    }
+};
+
+let clearInputField = () => {
+    document.getElementById("art").value = "";
+    document.getElementById("wert").value = "";
+    let myInput = document.getElementsByName('wert')[0];
+    myInput.placeholder='Wert';
+};
+
+let useColor = (kategorie, art, tableRowId) => {
+    let green = 'rgba(0, 255, 0, 0.5)';
+    let yellow = 'rgba(255, 255, 0, 0.5)';
+    let red = 'rgba(255, 0, 0, 0.5)';
+    let lightgrey = 'lightgrey';
+    let color = document.getElementById(tableRowId).style;
+
+    switch  (art){
+        case 'Gesamtkeimzahl':
+            if(kategorie === '<50000' || kategorie >= 1000 && kategorie < 50000){
+                color.backgroundColor = green;
+            }
+            else if(kategorie >= 50000 && kategorie < 5000000){
+                color.backgroundColor = yellow;
+            }
+            else if(kategorie >= 5000000 && kategorie <= 50000000){
+                color.backgroundColor = red;
+            } else {
+                color.backgroundColor = lightgrey;
+            }
+            break;
+
+        case 'E.coli':
+            if(kategorie === '<100' || kategorie > 0 && kategorie < 100){
+                color.backgroundColor = green;
+            }
+            else if(kategorie >= 100 && kategorie < 500){
+                color.backgroundColor = yellow;
+            }
+            else if(kategorie >= 500 && kategorie <= 5000){
+                color.backgroundColor = red;
+            } else {
+                color.backgroundColor = lightgrey;
+            }
+            break;
+
+        case 'Salmonellen':
+            if(kategorie === 'negativ'){
+                color.backgroundColor = green;
+            }
+            else if(kategorie === 'positiv'){
+                color.backgroundColor = red;
+            } else {
+                color.backgroundColor = lightgrey;
+            }
+            break;
+
+        case 'Listerien':
+            if(kategorie === 'negativ'){
+                color.backgroundColor = green;
+            }
+            else if(kategorie === 'positiv'){
+                color.backgroundColor = red;
+            } else {
+                color.backgroundColor = lightgrey;
+            }
+            break;
+
+        case 'koagulase positive Staphylokokken':
+            if(kategorie === '<100' || kategorie > 0 && kategorie < 100){
+                color.backgroundColor = green;
+            }
+            else if(kategorie >= 100 && kategorie < 500){
+                color.backgroundColor = yellow;
+            }
+            else if(kategorie >= 500 && kategorie <= 5000){
+                color.backgroundColor = red;
+            } else {
+                color.backgroundColor = lightgrey;
+            }
+            break;
+
+        case 'Enterobacteriaceae':
+            if(kategorie === '<1000' || kategorie > 100 && kategorie < 1000){
+                color.backgroundColor = green;
+            }
+            else if(kategorie >= 1000 && kategorie < 5000){
+                color.backgroundColor = yellow;
+            }
+            else if(kategorie >= 5000 && kategorie <= 50000){
+                color.backgroundColor = red;
+            } else {
+                color.backgroundColor = lightgrey;
+            }
+            break;
+        default:
+            document.getElementById("art").value = "";
+            document.getElementById("wert").value = "Wert";
+    }
+};
+
+export {
+    myPlaceholder,
+    time,
+    createInputTable,
+    clearInputField,
+    useColor
 };
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { myPlaceholder} from "./module.js";
+import { myPlaceholder, time, createInputTable, clearInputField, useColor} from "./module.js";
 
 let tableRowId = "1";
 
@@ -16,144 +16,14 @@ submitInput.addEventListener("keypress", (event) => {
 
 let submitButton = document.getElementById('submit');
 submitButton.addEventListener('click', () => {
-    farbe();
+    main();
 });
 
-function farbe(){
+let main = () => {
     let kategorie = document.getElementById('wert').value.toLowerCase();
     let art = document.getElementById('art').value;
-    let green = 'rgba(0, 255, 0, 0.5)';
-    let yellow = 'rgba(255, 255, 0, 0.5)';
-    let red = 'rgba(255, 0, 0, 0.5)';
-    let lightgrey = 'lightgrey';
-
-
-    const time = () => {
-        let timeElapsed = Date.now();
-        let time = new Date(timeElapsed);
-        let hour = time.getHours();
-        let min = time.getMinutes();
-        let sec = time.getSeconds();
-        if (hour < 10){
-            hour = `0${hour}`;
-        }
-        if (min < 10){
-            min = `0${min}`;
-        }
-        if (sec < 10){
-            sec = `0${sec}`;
-        }
-
-        return (`${hour}:${min}:${sec}`);
-    };
-    let createInputTable = (art, kategorie, time) =>{
-        if (art !== "" && kategorie !== ""){
-            let inputTable = document.getElementById('inputList');
-            let row = inputTable.insertRow(tableRowId);
-            row.id=tableRowId;
-            let cellOne = row.insertCell(0);
-            let cellTwo = row.insertCell(1);
-            let cellThree = row.insertCell(2);
-            cellOne.innerHTML = time();
-            cellTwo.innerHTML = art;
-            cellThree.innerHTML = kategorie;
-        }
-    };
-
-    createInputTable(art, kategorie, time);
-    let color = document.getElementById(tableRowId).style;
-    switch  (art){
-        case 'Gesamtkeimzahl':
-            if(kategorie === '<50000' || kategorie >= 1000 && kategorie < 50000){
-                color.backgroundColor = green;
-            }
-            else if(kategorie >= 50000 && kategorie < 5000000){
-                color.backgroundColor = yellow;
-            }
-            else if(kategorie >= 5000000 && kategorie <= 50000000){
-                color.backgroundColor = red;
-            } else {
-                color.backgroundColor = lightgrey;
-            }
-            break;
-
-        case 'E.coli':
-            if(kategorie === '<100' || kategorie > 0 && kategorie < 100){
-                color.backgroundColor = green;
-            }
-            else if(kategorie >= 100 && kategorie < 500){
-                color.backgroundColor = yellow;
-            }
-            else if(kategorie >= 500 && kategorie <= 5000){
-                color.backgroundColor = red;
-            } else {
-                color.backgroundColor = lightgrey;
-            }
-            break;
-
-        case 'Salmonellen':
-            if(kategorie === 'negativ'){
-                color.backgroundColor = green;
-            }
-            else if(kategorie === 'positiv'){
-                color.backgroundColor = red;
-            } else {
-                color.backgroundColor = lightgrey;
-            }
-            break;
-
-        case 'Listerien':
-            if(kategorie === 'negativ'){
-                color.backgroundColor = green;
-            }
-            else if(kategorie === 'positiv'){
-                color.backgroundColor = red;
-            } else {
-                color.backgroundColor = lightgrey;
-            }
-            break;
-
-        case 'koagulase positive Staphylokokken':
-            if(kategorie === '<100' || kategorie > 0 && kategorie < 100){
-                color.backgroundColor = green;
-            }
-            else if(kategorie >= 100 && kategorie < 500){
-                color.backgroundColor = yellow;
-            }
-            else if(kategorie >= 500 && kategorie <= 5000){
-                color.backgroundColor = red;
-            } else {
-                color.backgroundColor = lightgrey;
-            }
-            break;
-
-        case 'Enterobacteriaceae':
-            if(kategorie === '<1000' || kategorie > 100 && kategorie < 1000){
-                color.backgroundColor = green;
-            }
-            else if(kategorie >= 1000 && kategorie < 5000){
-                color.backgroundColor = yellow;
-            }
-            else if(kategorie >= 5000 && kategorie <= 50000){
-                color.backgroundColor = red;
-            } else {
-                color.backgroundColor = lightgrey;
-            }
-            break;
-        default:
-            document.getElementById("art").value = "";
-            document.getElementById("wert").value = "Wert";
-
-    }
-    let clearInputField = () => {
-        document.getElementById("art").value = "";
-        document.getElementById("wert").value = "";
-        let myInput = document.getElementsByName('wert')[0];
-        myInput.placeholder='Wert';
-
-
-    };
+    createInputTable(art, kategorie, time, tableRowId);
+    useColor(kategorie, art, tableRowId);
     clearInputField();
-    tableRowId++
-
+    tableRowId++;
 };
