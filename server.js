1068f63c7d1e41448e2646576e0aa499403c3ed7
Austriana <Marco.Mang@gmx.de>
2025-08-15 16:57:52 +0200

    ...

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import http from 'node:http';
 import { WebSocketServer, WebSocket } from 'ws';
 import {route} from './route.js'
 
-const PORT = process.env.PORT||80;
+const PORT = process.env.PORT||3500;
 
 const server = http.createServer(route);
 // ws ------------------------------------------------------------------
