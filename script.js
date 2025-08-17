73a6667e2bd9a5d86afe021219e1b44d61d7796b
Austriana <Marco.Mang@gmx.de>
2025-08-17 17:31:59 +0200

    update

--- script.js
+++ script.js
@@ -55,30 +55,3 @@ let main = () => {
     createInputRow(art, kategorie, time());
     clearInputField();
 };
-
-// switch ssl on local
-let url = "wss://" + location.host;
-if(location.host === 'localhost:3500'){
-  url = "ws://" + location.host;
-}
-let ws = new WebSocket(url);
-
-let toServerMsg = {
-  zahl1: 1,
-  zahl2: 2,
-  zahl3: 3,
-  zahl4: 4,
-  zahl5: 5,
-  zahl6: 6,
-  zahl7: 7,
-};
-
-let send = document.getElementById('send');
-send.addEventListener('click', () => {
-  ws.send(JSON.stringify(toServerMsg));
-});
-
-ws.addEventListener('message', (msg) => {
-  let fromServerMsg = msg.data;
-  console.log(JSON.parse(fromServerMsg))
-});
